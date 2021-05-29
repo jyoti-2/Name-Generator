e8ccc775b6034af06fb9a5057d813ff51053464a
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-05-29 11:15:59 +0530

    update package file

--- src/Components/Namecard/Namecard.jsx
+++ src/Components/Namecard/Namecard.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Namecard.css';
 
-
 const nameCheapurl = 'https://www.nameCheap.com/domains/registration/results/domain';
-
-
 const Namecard = ({Suggestednames}) => {
     return (
         <a href={`${nameCheapurl}${Suggestednames}`}>
